86fbdfda677ffb1245c31366e6078e1833ad2e49
alex <alex.algarate@gmail.com>
2025-02-11 16:51:34 +0100

    Refactor main

--- main.py
+++ main.py
@@ -8,10 +8,20 @@ from src.activities import ActivitiesManager
 def run_async_streams(access_token: str):
     print(
         asyncio.run(
-            ActivitiesManager.get_all_streams(
+            ActivitiesManager.get_multiple_activities_streams(
                 access_token,
                 utils.id_activities,
                 utils.streams_keys,
+            )
+        )
+    )
+
+
+def run_async_activity_range(access_token: str, previous_week: bool = False):
+    print(
+        asyncio.run(
+            ActivitiesManager(access_token).get_activity_range_async(
+                previous_week=previous_week
             )
         )
     )
@@ -25,26 +35,18 @@ def show_last_200_activities(access_token: str):
     print(ActivitiesManager(access_token).get_last_200_activities())
 
 
-def show_activities_from_current_week(access_token: str):
-    print(ActivitiesManager(access_token).get_activity_range())
-
-
-def show_activities_from_previous_week(access_token: str):
-    print(ActivitiesManager(access_token).get_activity_range(previous_week=True))
-
-
 def print_options():
-    print("\nElije una opción")
+    print("\Choose an option:")
 
-    print("\nStrava - Opciones:")
-    print("1. Mostrar la información de una actividad concreta")
-    print("2. Mostrar la información de las actividades de la semana en curso")
-    print("3. Mostrar las últimas 200 actividades")
-    print("4. Mostrar la información de las actividades de la semana anterior")
-    print("5. Mostrar los streams de las actividades de la semana en curso")
-    print("6. Mostrar un gráfico del tiempo en zona de una actividad en concreto")
-    print("7. Mostrar el historial de commits")
-    print("8. Salir")
+    print("\nStrava - Options:")
+    print("1. Show information for a specific activity")
+    print("2. Show information for activities from the current week")
+    print("3. Show the last 200 activities")
+    print("4. Show information for activities from the previous week")
+    print("5. Show streams for activities from the current week")
+    print("6. Show a graph of time in zone for a specific activity")
+    print("7. Show commit history")
+    print("8. Exit")
 
 
 def main():
@@ -52,33 +54,35 @@ def main():
 
     while True:
         print_options()
-        choice = input("Selecciona una opción (1 al 8): ")
+        choice = input("Choose an option: (1 to 8) ")
 
         match choice:
             case "1":
                 show_one_activity(access_token)
 
             case "2":
-                show_activities_from_current_week(access_token)
+                run_async_activity_range(access_token, previous_week=False)
 
             case "3":
                 show_last_200_activities(access_token)
 
             case "4":
-                show_activities_from_previous_week(access_token)
+                run_async_activity_range(access_token, previous_week=True)
 
             case "5":
-                print(run_async_streams(access_token))
+                run_async_streams(access_token)
 
             case "6":
-                print("NO HAS CORRIDO")
+                print("Feature not implemented yet.")
+
             case "7":
-                print("NO HAS CORRIDO")
+                print("Feature not implemented yet.")
+
             case "8":
-                print("Saliendo")
+                print("Goodbye!")
                 break
             case _:
-                print("Opción no válida.")
+                print("Not valid option.")
 
 
 if __name__ == "__main__":
