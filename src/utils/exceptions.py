840dd1cbaf7047a5fce5aa7411b0feb7e710a4c3
alex <alex.algarate@gmail.com>
2025-03-19 09:10:28 +0100

    Add custom exception classes for error handling

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -1,6 +1,18 @@
+class NotActivitiesError(Exception):
+    pass
+
+
+class TooManyRequestError(Exception):
+    pass
+
+
 class DatabaseOperationError(Exception):
     pass
 
 
 class TokenError(Exception):
+    pass
+
+
+class UnauthorizedError(Exception):
     pass
