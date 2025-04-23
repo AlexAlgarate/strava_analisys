5f2ba270b44059013d11d3bc1ecd2e67921783e3
alex <alex.algarate@gmail.com>
2025-04-23 12:39:35 +0200

    base strava api interface

--- src/interfaces/strava_api.py
+++ src/interfaces/strava_api.py
@@ -1,24 +1,64 @@
+# from abc import ABC, abstractmethod
+# from typing import Any, Dict
 from abc import ABC, abstractmethod
-from typing import Any, Dict
+from dataclasses import dataclass
+from typing import Dict
+
+from src.database.supabase_deleter import SupabaseDeleter
+from src.strava_api.http.base_http_client import BaseHTTPClient
+
+# class HTTPClient(ABC):
+#     @abstractmethod
+#     def get(
+#         self, url: str, headers: Dict[str, str], params: Dict[str, Any] = None
+#     ) -> Dict[str, Any]:
+#         pass
 
 
-class HTTPClient(ABC):
-    @abstractmethod
-    def get(
-        self, url: str, headers: Dict[str, str], params: Dict[str, Any] = None
-    ) -> Dict[str, Any]:
-        pass
+# class BaseStravaAPI(ABC):
+#     @abstractmethod
+#     def __init__(self, access_token: str, http_client: HTTPClient, config: Any = None):
+#         pass
+
+#     @abstractmethod
+#     def get_headers(self) -> Dict[str, str]:
+#         pass
+
+#     @abstractmethod
+#     def get_url(self, endpoint: str) -> str:
+#         pass
+
+
+@dataclass
+class StravaAPIConfig:
+    base_url: str = "https://www.strava.com/api/v3"
+    content_type: str = "application/json"
 
 
 class BaseStravaAPI(ABC):
-    @abstractmethod
-    def __init__(self, access_token: str, http_client: HTTPClient, config: Any = None):
-        pass
+    def __init__(
+        self,
+        access_token: str,
+        http_client: BaseHTTPClient,
+        deleter: SupabaseDeleter = None,
+        config: StravaAPIConfig = None,
+    ):
+        if not access_token:
+            raise ValueError("\n\nAccess token must be provided.")
+        self.access_token = access_token
+        self.http_client = http_client
+        self.config = config or StravaAPIConfig()
+        self.deleter = deleter
 
     @abstractmethod
-    def get_headers(self) -> Dict[str, str]:
-        pass
+    def get_headers(self, access_token: str, content_type: str) -> Dict[str, str]: ...
 
-    @abstractmethod
+    # @abstractmethod
+    # def get_headers(self,access_token:str,content_type:str) -> Dict[str, str]:
+    #     return {
+    #         "Authorization": f"Bearer {self.access_token}",
+    #         "Content-Type": self.config.content_type,
+    #     }
+
     def get_url(self, endpoint: str) -> str:
-        pass
+        return f"{self.config.base_url}{endpoint}"
--- src/strava_api/api/sync_strava_api.py
+++ src/strava_api/api/sync_strava_api.py
@@ -1,5 +1,9 @@
+from typing import Dict
+
+from src.interfaces.strava_api import BaseStravaAPI
+
 from ..http.http_clients import SyncHTTPClient
-from .base_strava_api import BaseStravaAPI, StravaAPIConfig
+from .base_strava_api import StravaAPIConfig
 
 
 class SyncStravaAPI(BaseStravaAPI):
@@ -8,6 +12,14 @@ class SyncStravaAPI(BaseStravaAPI):
             access_token=access_token, http_client=SyncHTTPClient(), config=config
         )
 
+    def get_headers(self) -> Dict[str, str]:
+        return {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": self.config.content_type,
+        }
+
     def make_request(self, endpoint: str, params: dict = None) -> dict:
         url = self.get_url(endpoint)
-        return self.http_client.get(url, headers=self.get_headers(), params=params)
+        headers = self.get_headers()
+
+        return self.http_client.get(url, headers=headers, params=params)
