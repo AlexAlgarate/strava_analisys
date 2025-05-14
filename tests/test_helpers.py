e96bef3e8a54653a38f0680e5587c4f9477fbf26
alex <alex.algarate@gmail.com>
2025-05-14 12:37:13 +0200

    Enhance type hints for test helpers

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -7,7 +7,7 @@ from src.utils.helpers import func_time_execution, get_week_epoch_range
 
 
 class TestEpochTimeCalculation:
-    def test_get_epoch_time_beginning_of_week(self):
+    def test_get_epoch_time_beginning_of_week(self) -> None:
         # Sunday
         with freeze_time("2024-01-21"):
             monday, sunday = get_week_epoch_range()
@@ -15,7 +15,7 @@ class TestEpochTimeCalculation:
             assert isinstance(sunday, int)
             assert monday < sunday
 
-    def test_get_epoch_time_middle_of_week(self):
+    def test_get_epoch_time_middle_of_week(self) -> None:
         # Wednesday
         with freeze_time("2024-01-24"):
             monday, sunday = get_week_epoch_range()
@@ -23,7 +23,7 @@ class TestEpochTimeCalculation:
             assert isinstance(sunday, int)
             assert monday < sunday
 
-    def test_get_epoch_time_end_of_week(self):
+    def test_get_epoch_time_end_of_week(self) -> None:
         # Saturday
         with freeze_time("2024-01-27"):
             monday, sunday = get_week_epoch_range()
@@ -31,7 +31,7 @@ class TestEpochTimeCalculation:
             assert isinstance(sunday, int)
             assert monday < sunday
 
-    def test_get_epoch_time_previous_week(self):
+    def test_get_epoch_time_previous_week(self) -> None:
         # Current week
         with freeze_time("2024-01-24"):
             current_monday, _ = get_week_epoch_range()
@@ -44,9 +44,9 @@ class TestEpochTimeCalculation:
 
 
 @pytest.mark.asyncio
-async def test_func_time_execution_decorator():
+async def test_func_time_execution_decorator() -> None:
     @func_time_execution
-    async def sample_async_function():
+    async def sample_async_function() -> str:
         await asyncio.sleep(0.1)
         return "test"
 
